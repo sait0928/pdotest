702f7fbc994fdbb6ba54a3bd5c10af656d39e948
sait0928 <sait0yuya928@gmail.com>
2019-01-30 14:06:09 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja-jp">
+<head>
+	<title>記事投稿CMS</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<section>
+		<h2>NEWS</h2>
+		<article><a href="#">
+				<p>NEWS TITLE</p>
+			</a></article>
+		<article><a href="#">
+				<p>NEWS TITLE</p>
+			</a></article>
+		<article><a href="#">
+				<p>NEWS TITLE</p>
+			</a></article>
+		<article><a href="#">
+				<p>NEWS TITLE</p>
+			</a></article>
+		<article><a href="#">
+				<p>NEWS TITLE</p>
+			</a></article>
+	</section>
+
+	<section>
+		<form method="post" action="form.php">
+			<input type="text" name="title" id="">
+			<textarea name="text" id="" cols="30" rows="10"></textarea>
+			<input type="submit" value="送信">
+		</form>
+	</section>
+</body>
+</html>
